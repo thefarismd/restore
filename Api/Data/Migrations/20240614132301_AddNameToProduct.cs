b2ee2d2b92eea031b663ca80c36f3c714e4f7fa6
thefarismd <thefarismd@icloud.com>
2024-06-14 23:21:49 +0800

    intial commit

---
+++ Api/Data/Migrations/20240614132301_AddNameToProduct.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNameToProduct : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "QuantityInStock",
+                table: "Products",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuantityInStock",
+                table: "Products",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+        }
+    }
+}
